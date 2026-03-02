c7acaad0e1842a9a1ee3d3cb2a95c56d08c50979
CompSciHigh24 <csh-github@compscihigh.org>
2026-03-02 19:13:16 +0000

    v1

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+// 1) Use npm to install ejs in the terminal
 
+
+// 2) create a views and a public folder
+
+
+
+// 3) In your public folder create an index.html and add your code from the planning document 
+// You can also add your CSS files here too if applicable 
+
+
+
+
+// 4) Now in your views folder create your EJS file and add your HTML code from your planning document
+
+
+
+const express = require('express')
+const app = express()
+
+app.use((req, res, next) => {
+    console.log(req.method, req.url)
+    next()
+})
+
+
+// 5) Paste in your data array of objects from the planning documents here. 
+
+
+// 6) Add static file middleware here
+
+
+// 7) Set the view engine to ejs here
+
+
+// 8) Create a route handler to path / and send your index.html 
+// run your server with node index.js to test it
+
+
+
+// 9) Create a route handler to your overview route from your planning document
+// Pass in your data array
+
+
+
+
+// 10) Go to your EJS file and turn the HTML into EJS
+// Test and run your server then submit if working (git add . git commit -m 'message' git push)
+
+
+app.listen(3000, ()=> {
+    console.log("Server Started")
+})
